823f10190540cbba83b5a17f1eaf943e029a711c
RenasAli <renas.xllo1999@gmail.com>
2025-11-03 13:14:19 +0100

    add GTM tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,15 +44,49 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const GTM_ID = "GTM-NDWQDJ4T";
   return (
     <html lang="en">
       <head>
+        {/* Consent Mode default  */}
+        <Script id="gtm-consent-default" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('consent', 'default', {
+              'ad_storage': 'denied',
+              'analytics_storage': 'denied',
+              'ad_user_data': 'denied',
+              'ad_personalization': 'denied',
+              'wait_for_update': 500
+            });
+          `}
+        </Script>
+
+        {/* GTM */}
+        {GTM_ID && (
+          <Script id="gtm-base" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `}
+          </Script>
+        )}
+
         {/* Cookiebot CMP */}
         <Script 
           id="Cookiebot"
           src="https://consent.cookiebot.com/uc.js" data-cbid="588a22c3-78eb-4d69-abe7-568c80ef1efc"
           data-blockingmode="auto" type="text/javascript"
-        ></Script>
+        >
+        </Script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Providers >
