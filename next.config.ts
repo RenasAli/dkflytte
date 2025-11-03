386f8f8551e9ec1c0ebc9403853e5fd346951af2
RenasAli <renas.xllo1999@gmail.com>
2025-11-03 17:03:22 +0100

    the last result

--- next.config.ts
+++ next.config.ts
@@ -1,42 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/privatflytning',
-        destination: '/private-moving'
-      },
-      {
-        source: '/erhvervsflytning',
-        destination: '/business-moving'
-      },
-      {
-        source: '/flytteservice',
-        destination: '/moving-service'
-      },
-      {
-        source: '/landsdaekkende-og-internationale-flytninger',
-        destination: '/long-distance-moving'
-      },
-      {
-        source: '/ekstra-services-og-tryghedspakker',
-        destination: '/extra-services'
-      },
-      {
-        source: '/kontakt',
-        destination: '/contact'
-      },
-      {
-        source: '/priser',
-        destination: '/pricing'
-      },
-      {
-        source: '/om-os',
-        destination: '/about'
-      },
-    ];
-  },
+  output: "export",
+  trailingSlash: true,
+  outputFileTracingRoot: __dirname,
 };
 
 export default nextConfig;
